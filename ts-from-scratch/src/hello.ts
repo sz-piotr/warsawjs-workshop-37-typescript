9adba39194d01d1a2e498eb7ec8f46e51a0fe353
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-10-27 17:46:35 +0100

    More config

---
+++ ts-from-scratch/src/hello.ts
@@ -0,0 +1,7 @@
+export function hello (person: string) {
+  console.log('Hello ' + person)
+}
+
+export function throwError () {
+  throw new Error('Error')
+}
---
+++ ts-from-scratch/src/index.ts
@@ -0,0 +1,5 @@
+export * from './hello'
+
+export function add (a: number, b: number) {
+  return a + b
+}
